7dba9d5a410ab6700798bcf3fb0ef11ef72a7e4a
Draps- <dan@aussymate.com>
2015-02-09 21:24:34 +1100

    Added a quick work around menu. Will add a state command model in a future revision. Added word wrapping too neaten up the aggrigated answers.

--- nani/cmdline.py
+++ nani/cmdline.py
@@ -1,5 +1,6 @@
 import argparse
 from fetcher import Fetcher
+import textwrap
 
 def run():
 	parser = argparse.ArgumentParser(prog='nani',
@@ -10,7 +11,57 @@ def run():
 					help='The number of results to return')
 	args = parser.parse_args()
 	
+	exit = False
 	fetch = Fetcher()
 	fetch.fetch_search(args.search, args.results)
+	focus = None
+
+	exits = ['q','Q','Quit','quit','exit','Exit','done','Done']
+	opens = ['o','O','Open','open']
+	backs = ['b','B','Back','back']
+	helps = ['h','H','Help','help']
+
+	help = '''
+Help:\n
+===Search List===\n
+\t<number> - Select a search result\n
+\tquit - Quit the Nani search\n
+\t\tAliases: q, quit, exit, done\n
+\thelp - This help output\n
+\t\tAliases: h, help\n
+===Answer List===\n
+\topen - Open this page in your default browser\n
+\t\tAliases: o, open\n
+\tback - Go back to the Search List\n
+\t\tAliases: b, back\n'''
+
+	while not exit:
+		print(fetch.format_output(focus))
+		in_string = input(">")
+		if focus != None:
+			if in_string in opens:
+				fetch.open_page(focus)
+			elif in_string in backs:
+				focus = None
+			elif in_string in exits:
+				exit = True
+			else:
+				print('Unrecognised Command. back OR open')
+		else:
+			if in_string in helps:
+				print(help)
+			elif in_string in exits:
+				exit = True
+			else:
+				try:
+					focus = int(in_string)
+					if focus > fetch.max_results or focus < 1:
+						print('No Matching Result!')
+						focus = None
+					else:
+						focus = focus-1
+				except:
+					print('Unrecognised command, Use help')
+					focus = None
 
 run()
--- nani/fetcher.py
+++ nani/fetcher.py
@@ -1,6 +1,7 @@
 import mechanicalsoup
 from urllib.parse import urlparse
-import traceback
+import webbrowser
+import textwrap
 import interfaces
 
 class Fetcher():
@@ -8,6 +9,7 @@ class Fetcher():
 	def __init__(self):
 		self.fetched = [] #turple of (Title, Url)
 		self.results = [] #turple of (Question, Answers[*], Ratings[*])
+		self.max_results = 0
 
 	def fetch_search(self, search, results):
 		browser = mechanicalsoup.Browser()
@@ -20,10 +22,14 @@ class Fetcher():
 		#get the links from the results
 		if search_list:
 			for item in search_list:
-				fixed_url = urlparse(item.parent.a['href'][7:]).geturl().split('&')[0]
-				self.fetched.append((item.text,fixed_url))
-				#print(urlparse(item.parent.a['href'][7:]).geturl())
+				if item.parent.a != None:
+					parsed_url = urlparse(item.parent.a['href'][7:])
+					if parsed_url.netloc != '':
+						fixed_url = parsed_url.geturl().split('&')[0]
+						self.fetched.append((item.text,fixed_url))
+						#print(urlparse(item.parent.a['href'][7:]).geturl())
 			
+			new_fetched = []
 			for item in self.fetched:
 				lookup = urlparse(item[1]).netloc
 				lookup = lookup.replace('www.', '')
@@ -34,21 +40,36 @@ class Fetcher():
 				try:
 					interface = eval('interfaces.'+lookup+'()')
 					self.results.append(interface.analyze_page(browser.get(item[1])))
+					new_fetched.append(item)
 		
 				except AttributeError:
 					print('No Interface OR Incomplete Interface for '+lookup)
-
-				except Exception:
-					print(traceback.format_exc())
-			
-			for item in self.results:
-				print('\nQUESTION:')				
-				print(item[0] + '\nANSWERS:')
-				count = 1
-				for post, rating in zip(item[1],item[2]):				
-					print('ANSWER '+str(count)+'('+str(rating)+'):\n'+post)
-					count += 1
+					
+			self.fetched = new_fetched
+			self.max_results = len(self.results)
 			
 
-	def build_results():
-		return "Incomplete"
+	def format_output(self, focus=None):
+		wrapper = textwrap.TextWrapper()
+		wrapper.width = 70
+		wrapper.replace_whitespace = False
+		output = '\n'
+		if focus == None:
+			count = 1
+			for search_item in self.fetched:
+				output += '[+] '+str(count)+'. '+search_item[0]+'\n'
+				output += ' - '+search_item[1]+'\n\n'
+				count += 1
+		else:
+			item = self.results[focus]
+			output += '\n[*] QUESTION:\n'				
+			output += item[0] + '\n[*] ANSWERS:\n'
+			count = 1
+			for post, rating in zip(item[1],item[2]):				
+				output += '[+] ANSWER '+str(count)+' Rating('+str(rating)+'):\n'+wrapper.fill(post)+'\n'
+				count += 1
+
+		return output
+
+	def open_page(self, index):
+		webbrowser.open(self.fetched[index][1])
